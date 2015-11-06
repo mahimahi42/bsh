7b0559e156c0f0191e5793f854d142abce7e7152
Bryce Davis <me@bryceadavis.com>
2015-11-06 15:58:11 -0500

    Add provisioning file for development environment

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+sudo yum -y update
+sudo yum -y groupinstall 'Development Tools'
+sudo yum -y install git
+git config --global user.name "Bryce Davis"
+git config --global user.email me@bryceadavis.com
