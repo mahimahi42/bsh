da2d8915f6ad309808f48f5eefaa89484b4722f4
Bryce Davis <me@bryceadavis.com>
2015-11-06 15:59:11 -0500

    Add vim and zsh to development environment

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 sudo yum -y update
 sudo yum -y groupinstall 'Development Tools'
-sudo yum -y install git
+sudo yum -y install git vim zsh
 git config --global user.name "Bryce Davis"
 git config --global user.email me@bryceadavis.com
