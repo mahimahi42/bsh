50840f89bd828cede881e59c0ce16b046f9f7e23
Bryce Davis <me@bryceadavis.com>
2015-11-06 23:17:09 -0500

    Add tokenizing function to I/O utilities

--- src/io.c
+++ src/io.c
@@ -72,5 +72,33 @@ char* bsh_read_line(void)
 */
 char** bsh_split_line(char* line)
 {
-	
+	size_t bufsize = BSH_TOK_BUF_SIZE;
+	int pos = 0;
+
+	char** tokens = malloc(bufsize * sizeof(char*));
+	char* curr_tok;
+
+	if (!tokens) {
+		fprintf(stderr, "bsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	curr_tok = strtok(line, BSH_TOK_DELIM);
+	while (curr_tok != NULL) {
+		tokens[pos] = curr_tok;
+		pos++;
+
+		if ((size_t) pos >= bufsize) {
+			bufsize <<= 1;
+			tokens = realloc(tokens, bufsize * sizeof(char*));
+			if (!tokens) {
+				fprintf(stderr, "bsh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		curr_tok = strtok(NULL, BSH_TOK_DELIM);
+	}
+	tokens[pos] = NULL;
+	return tokens;
 }
