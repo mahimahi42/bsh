42e53c0292eda46401f893751c989d35fd746190
Bryce Davis <me@bryceadavis.com>
2015-11-07 02:41:05 -0500

    Update bootstrap file for Vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,28 @@
 sudo yum -y update
 sudo yum -y groupinstall 'Development Tools'
 sudo yum -y install git vim zsh
-git config --global user.name "Bryce Davis"
-git config --global user.email me@bryceadavis.com
+
+cat > /home/vagrant/.gitconfig << EOF
+[user]
+	name = Bryce Davis
+	email = me@bryceadavis.com
+[core]
+	editor = vim
+[alias]
+	co = checkout
+	st = status
+	ci = commit
+	br = branch
+	last = log -1 HEAD
+	g = log --graph --oneline --decorate
+	df = diff
+
+EOF
+
+cat > /home/vagrant/.zshrc << EOF
+# Created by newuser for 5.0.2
+cd /vagrant
+
+alias gitc="git commit -am"
+
+EOF
